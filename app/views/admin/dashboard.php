b5e1e87d5b3bf8f8b918be41ef970c81e5b25acc
AbelArdhana <abelardhana96@gmail.com>
2019-10-27 10:34:37 +0800

    menambah link public + test konek

---
+++ app/views/admin/dashboard.php
@@ -0,0 +1,4 @@
+<?php include '../templates/header.php'; ?>
+<h1>HAHAHA, TEST KONEK PUBLIC</h1>
+<h2>ASIK ASIK JOS, KONEK JI</h2>
+<?php include '../templates/footer.php' ?>
---
+++ app/views/templates/footer.php
@@ -0,0 +1,15 @@
+<!-- CORE -->
+<script src="../../../public/vendor/jquery/jquery.min.js"></script>
+<script src="../../../public/vendor/popper/popper.min.js"></script>
+<script src="../../../public/vendor/bootstrap/bootstrap.min.js"></script>
+<script src="../../../public/vendor/headroom/headroom.min.js"></script>
+<!-- OPTIONAL -->
+<script src="../../../public/vendor/onscreen/onscreen.min.js"></script>
+<script src="../../../public/vendor/nouislider/js/nouislider.min.js"></script>
+<script src="../../../public/vendor/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script>
+<!-- ARGON -->
+<script src="../../../public/js/argon.min.js"></script>
+
+</body>
+
+</html>
