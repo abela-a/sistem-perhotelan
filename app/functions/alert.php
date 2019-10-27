1702553333b7ee8c89532b6b7c6c5d364076ce43
AbelArdhana <abelardhana96@gmail.com>
2019-10-28 05:55:32 +0800

    alert auto close

--- app/functions/alert.php
+++ app/functions/alert.php
@@ -13,7 +13,7 @@ function Alert()
 {
   if (isset($_SESSION['alert'])) {
     echo '
-    <div class="alert alert-' . $_SESSION['alert']['tipe'] . ' alert-dismissible fade show" role="alert">
+    <div class="alert alert-' . $_SESSION['alert']['tipe'] . ' alert-dismissible sticky-top fade show" role="alert">
     <span class="alert-inner--icon"><i class="' . $_SESSION['alert']['icon'] . ' fa-fw"></i></span>
       <span class="alert-inner--text">' . $_SESSION['alert']['pesan'] . '</span>
       <button type="button" class="close" data-dismiss="alert" aria-label="Close">
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
   $("#datatables").DataTable();
+
+  // $(".alert")
+  //   .fadeTo(2000, 500)
+  //   .slideUp(500, function() {
+  //     $(this).slideUp(500);
+  //   });
 });
